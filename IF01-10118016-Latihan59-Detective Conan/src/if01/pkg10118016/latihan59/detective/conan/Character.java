e29dff651729ad44957a3506898d4e60ddcc55a3
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-11-05 09:57:54 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan59-Detective Conan/src/if01/pkg10118016/latihan59/detective/conan/Character.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan59.detective.conan;
+
+/**
+ *
+ * @author Rezky
+ */
+public class Character {
+    protected String name;
+    protected String from;
+    protected boolean eyeglasses;
+    
+    public Character(String name, String from, boolean eyeglasses) {
+        this.name = name;
+        this.from = from;
+        this.eyeglasses = eyeglasses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+   
+}
---
+++ IF01-10118016-Latihan59-Detective Conan/src/if01/pkg10118016/latihan59/detective/conan/IF0110118016Latihan59DetectiveConan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan59.detective.conan;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Detective Conan
+ */
+
+public class IF0110118016Latihan59DetectiveConan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainCharacter conan = new MainCharacter("Investigator", "Menyelesaikan Kasus","Conan Edogawa","Tokyo",true);
+        MainCharacter ran = new MainCharacter("Karate", "Sama seperti Shinichi","Ran Mouri","Osaka",false);
+        SupportCharacter eisuke = new SupportCharacter("Teman Sekelas Ran Mouri","Eisuke Hondo","Osaka",false);
+        conan.displayCharacter();
+        ran.displayCharacter();
+        eisuke.displayCharacter();
+    }
+    
+}
